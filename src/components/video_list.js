5301117d45c4ac2c02be5921ca1008a0b70ef81c
uditkumawat <udit.kumawat@gmail.com>
2017-09-12 00:17:04 +0530

    vide list and detail component added

---
+++ src/components/video_list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import VideoListItem from './video_list_item.js';
+
+const VideoList = (props)=>{
+
+    const videoItems = props.videos.map((video)=>{
+        return <VideoListItem video={video} key={video.etag}/>
+    });
+
+    return (
+        <ul className="col-md-4 list-group">
+            {videoItems}
+        </ul>
+    );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
 
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTsearch from 'youtube-api-search';
 //own components
 import SearchBar from './components/search_bar.js';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail.js';
 
 //youtube API KEY from console.developers.google.com
 
 const API_KEY = 'AIzaSyCuj_PhRhYHrxMrDbG-9Y6RYxQnxLv0q4E';
 
-//component
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
-}
 
-//take this HTML and render to page
+//component
+class App extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state={videos:[]};
+
+        YTsearch({key:API_KEY,term:'Udit'},(videos) => {
+            this.setState({videos});
+        });
+    }
+
+    render() {
+        return (
+            < div >
+            < SearchBar / >
+            <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    };
+};
+
 ReactDOM.render(<App />,document.querySelector(".container"));
